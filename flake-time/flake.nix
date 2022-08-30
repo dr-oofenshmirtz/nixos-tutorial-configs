98ed689d0ef73af825b8a679606cb521c527cef0
dr-oofenshmirtz <past.song6353@fastmail.com>
2022-08-30 00:30:02 -0700

    flaaaake

---
+++ flake-time/flake.nix
@@ -0,0 +1,26 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+  };
+
+  outputs = { self, nixpkgs }: # notice no curly brackets here because we use vars
+    let
+      system = "x86_64-linux"; # this isn't strictly nessecary,
+      # but it would be a pain not to
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;  # allows non-foss 
+      }
+      lib = nixpkgs.lib;
+      #hostname = "yolo"
+    in {
+      nixosConfigurations = {
+        yolo = lib.nixosSystem {
+          inherit system;
+          modules = [ ./configuration.nix ];
+        };
+      };
+    };
+  }
