12c6428aa1a8997aa6ade8b9bfa8c2f498a62875
dr-oofenshmirtz <past.song6353@fastmail.com>
2022-08-30 21:10:56 -0700

    fuck dude why

--- flake-home-manager-module/flake.nix
+++ flake-home-manager-module/flake.nix
@@ -2,9 +2,9 @@
   description = "A very basic flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager = {
-      url = github:nix-community/home-manager/archive/release-22.05.tar.gz;
+      url = github:nix-community/home-manager/archive/master.tar.gz;
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
