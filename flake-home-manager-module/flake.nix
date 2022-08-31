502c4508f2a212717c63532ad723d896edf1bca1
dr-oofenshmirtz <past.song6353@fastmail.com>
2022-08-30 17:59:11 -0700

    actually

---
+++ flake-home-manager-module/flake.nix
@@ -0,0 +1,39 @@
+{
+  description = "A very basic flake";
+
+  inputs = {
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-22.05";
+    home-manager = {
+      url = github:nix-community/home-manager;
+      inputs.nixpkgs.follows = "nixpkgs";
+    }
+  };
+
+  outputs = { self, nixpkgs, home-manager }: # notice no curly brackets here because we use vars
+    let
+      system = "x86_64-linux"; # this isn't strictly nessecary,
+      # but it would be a pain not to
+      pkgs = import nixpkgs {
+        inherit system;
+        config.allowUnfree = true;  # allows non-foss 
+      };
+      lib = nixpkgs.lib;
+      #hostname = "yolo"
+    in {
+      nixosConfigurations = {
+        yolo = lib.nixosSystem {
+          inherit system;
+          modules = [
+            ./configuration.nix
+            home-manager.nixosModules.home-manager {
+              home-manager.useGlobalPkgs = true;
+              home-manager.useUserPackages = true;
+              home-manager.users.yolo = {
+                imports = [ ./home.nix ];
+              };
+            }
+          ];
+        };
+      };
+    };
+  }
